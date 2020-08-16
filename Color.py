5b13ada6e994b6531e21468bc70a953ba20e714f
Diego <di-diego@javeriana.edu.co>
2020-08-15 22:17:59 -0500

    Código comentado

--- Color.py
+++ Color.py
@@ -2,43 +2,44 @@ import numpy as np
 import cv2
 import os
 
-class colorImage:
-    def __init__(self,path_file):
-        self.image = cv2.imread(path_file,1)
+class colorImage: #se crea la clase colorImage
 
-    def displayProperties(self):
-        print(f'El alto de la imagen es {self.image.shape[0]}. El ancho de la imagen es {self.image.shape[1]}')
+    def __init__(self,path_file): #se define el constructor
+        self.image = cv2.imread(path_file,1) #se carga la imagen (via OpenCV) y se almacena en self
 
-    def makeGray(self):
-        cv2.imshow('Gray Image',cv2.cvtColor(self.image,cv2.COLOR_BGR2GRAY))
+    def displayProperties(self): #metodo para mostrar tamaño de la imagen
+        print(f'El alto de la imagen es {self.image.shape[0]}. El ancho de la imagen es {self.image.shape[1]}') #se utiliza la funcion shape que es una lista donde se encuentra en la primera posicion el alto y en la segunda el ancho
+
+    def makeGray(self): #metodo para mostrar la imagen en escala de grises
+        cv2.imshow('Gray Image',cv2.cvtColor(self.image,cv2.COLOR_BGR2GRAY)) #se utiliza la funcion cv2.cvtColor() cuyos parámetros son la imagen y el espacio de color que se quiere usar, en nuestro caso se quiere pasar de BGR a Gray por lo que se utiliza cv2.COLOR_BGR2GRAY
         cv2.waitKey(0)
 
-    def colorizeRGB(self,canal_de_color):
-        if canal_de_color == 'blue':
-            b = self.image.copy()
-            b[:,:,1] = 0
-            b[:,:,2] = 0
-            cv2.imshow('Blue image',b)
+    def colorizeRGB(self,canal_de_color): #metodo para mostrar imagen colorizada con un respectivo canal de color
+        if canal_de_color == 'blue': #condicional que verifica si el canal de color deseado es azul
+            b = self.image.copy() #se copia la imagen original en la variable b
+            b[:,:,1] = 0 #se colocan las componentes verdes en cero
+            b[:,:,2] = 0 #se colocan las componentes rojas en cero
+            cv2.imshow('Blue image',b) #se muestra la imagen
             cv2.waitKey(0)
 
-        if canal_de_color == 'red':
-            r = self.image.copy()
-            r[:,:,0] = 0
-            r[:,:,1] = 0
-            cv2.imshow('Red image',r)
+        if canal_de_color == 'red':#condicional que verifica si el canal de color deseado es rojo
+            r = self.image.copy() #se copia la imagen original en la variable r
+            r[:,:,0] = 0 #se colocan las componentes azules en cero
+            r[:,:,1] = 0 #se colocan las componentes verdes en cero
+            cv2.imshow('Red image',r) #se muestra la imagen
             cv2.waitKey(0)
 
-        if canal_de_color == 'green':
-            g = self.image.copy()
-            g[:,:,0] = 0
-            g[:,:,2] = 0
-            cv2.imshow('Green image',g)
+        if canal_de_color == 'green': #condicional que verifica si el canal de color deseado es verde
+            g = self.image.copy() #se copia la imagen original en la variable g
+            g[:,:,0] = 0 #se colocan las componentes azules en cero
+            g[:,:,2] = 0 #se colocan las componentes rojas en cero
+            cv2.imshow('Green image',g) #se muestra la imagen
             cv2.waitKey(0)
 
-    def makeHue(self):
-        HSV_Image = cv2.cvtColor(self.image,cv2.COLOR_BGR2HSV)
-        HSV_Image[:,:,1] = 255
-        HSV_Image[:,:,2] = 255
-        HSV_Image = cv2.cvtColor(HSV_Image,cv2.COLOR_HSV2BGR)
-        cv2.imshow('Imagen en tonos',HSV_Image)
+    def makeHue(self): #metodo para mostrar imagen en tonos
+        HSV_Image = cv2.cvtColor(self.image,cv2.COLOR_BGR2HSV) #se guarda en HSV_Image la imagen original en el espacio de color HSV, por lo que se utiliza la funcion cv2.cvtColor() cuyos parámetros son la imagen y el espacio de color que se quiere usar
+        HSV_Image[:,:,1] = 255 #se coloca la componentes S en 255
+        HSV_Image[:,:,2] = 255 #se coloca la componente V en 255
+        HSV_Image = cv2.cvtColor(HSV_Image,cv2.COLOR_HSV2BGR) #se transforma el espacio de color de HSV a BGR
+        cv2.imshow('Imagen en tonos',HSV_Image) #se muestra la imagen
         cv2.waitKey(0)
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from  Color import *
+from  Color import * #se importa Color
 
-#C:\lenna.png
-
-if __name__ == '__main__':
-    ruta_imagen = input('Ingrese la ruta de la imagen: ')
-    imagen = colorImage(ruta_imagen)
-    imagen.displayProperties()
-    imagen.makeGray()
-    imagen.colorizeRGB('red')
-    imagen.makeHue()
-   
+if __name__ == '__main__': #
+    ruta_imagen = input('Ingrese la ruta de la imagen: ') #se pide al usuario que ingrese la ruta completa de la imagen
+    imagen = colorImage(ruta_imagen) #se hace un llamado a la clase y se ingresa como parámetro la ruta
+    imagen.displayProperties() #se muestran las propiedades de la imagen
+    imagen.makeGray() #se muestra la imagen en escala de grises
+    imagen.colorizeRGB('red') #se muestra la imagen colorizada en rojo
+    imagen.makeHue() #se muestra la imagen en tonos
